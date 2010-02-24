d161cbd180d963228e55f4c21b366a4db2db7ce8
Tim Harper <timcharper@gmail.com>
2010-02-23 17:56:03 -0800

    manager no longer pulled it's weight

--- lib/gitty/commands/add.rb
+++ lib/gitty/commands/add.rb
@@ -1,16 +1,24 @@
-require GITTY_PATH + "commands/manager"
-
-class Gitty::HookCommand::Add < Gitty::HookCommand::Manager
+class Gitty::HookCommand::Add < Gitty::Runner
   include FileUtils
+  def initialize(args, stdout = STDOUT, stderr = STDERR)
+    super
+    @hookname = args.shift
+  end
 
   def run
     hook = Gitty::Hook.find(@hookname, :installed => false)
     hook.install(options[:kind])
   end
 
+  def options
+    @options ||= super.update(:kind => :local)
+  end
+
   def option_parser
     @option_parser ||= super.tap do |opts|
       opts.banner = "Usage: git hook add [opts] hook-name"
+      opts.on("-l", "--local", "Local hook (default)") { |l| options[:kind] = :local }
+      opts.on("-s", "--shared", "Remote hook") { |l| options[:kind] = :shared }
     end
   end
 end
--- lib/gitty/commands/remove.rb
+++ lib/gitty/commands/remove.rb
@@ -1,14 +1,24 @@
-require GITTY_PATH + "commands/manager"
-class Gitty::HookCommand::Remove < Gitty::HookCommand::Manager
+class Gitty::HookCommand::Remove < Gitty::Runner
   include FileUtils
+  def initialize(args, stdout = STDOUT, stderr = STDERR)
+    super
+    @hookname = args.shift
+  end
+
   def run
     hook = Gitty::Hook.find(@hookname, :install_kind => options[:kind])
     hook.uninstall
   end
 
+  def options
+    @options ||= super.update(:kind => :local)
+  end
+
   def option_parser
     @option_parser ||= super.tap do |opts|
       opts.banner = "Usage: git hook remove [opts] hook-name"
+      opts.on("-l", "--local", "Local hook (default)") { |l| options[:kind] = :local }
+      opts.on("-s", "--shared", "Remote hook") { |l| options[:kind] = :shared }
     end
   end
 end
